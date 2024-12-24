b7d220027645e7def0235e5d48ad62d4cf6891dc
Federico Montoya <federicasiu.123@gmail.com>
2024-12-24 23:55:04 +0000

    se llama la funcion de otra manera

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
+import mean_var_std as st
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+result=st.calculate([2,6,2,8,4,0,1,5,7])
+
+print(result)
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
